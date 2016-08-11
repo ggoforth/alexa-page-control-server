61c4432ed78f0eadaaa13fe92c071ea35f1b282a
Greg Goforth <greg.goforth@gmail.com>
2016-08-10 22:18:11 -0700

    fix zoom for new supes image

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,5 +1,5 @@
-// var socket = io.connect('http://localhost:5000');
-var socket = io.connect('http://alexaman.shift3sandbox.com:5000'),
+var socket = io.connect('http://localhost:5000'),
+// var socket = io.connect('http://alexaman.shift3sandbox.com:5000'),
   $i = $('img'),
   $w = $(window);
 
@@ -8,40 +8,38 @@ var socket = io.connect('http://alexaman.shift3sandbox.com:5000'),
  */
 socket.on('move', function (data) {
   if (data.left > $w.width() - $i.width()) {
-    data.left = $w.width() - $i.width(); 
-  }  
-  
+    data.left = $w.width() - $i.width();
+  }
+
   if (data.top > $w.height() - $i.height()) {
     data.top = $w.height() - $i.height();
   }
-  
+
   $i.animate(data);
 });
 
 socket.on('reset', function () {
-  window.location.reload(true); 
+  window.location.reload(true);
 });
 
 /**
  * The Zoom event.
  */
 socket.on('zoom', (function () {
-  var scale = '120%';
+  var $scaled = $i.clone(),
+    scaling = 1.7;
 
-  var $scaled = $i.clone();
   $scaled.css({
     position: 'absolute',
     top: '-10000px',
     left: '-10000px',
-    height: scale,
-    width: scale
+    height: $i.height() * scaling,
+    width: $i.width() * scaling
   });
 
   $('body').append($scaled);
 
   return function (data) {
-    console.log('running zoom');
-    
     var height = $w.height(),
       width = $w.width(),
       iHeight = $i.height(),
@@ -56,29 +54,24 @@ socket.on('zoom', (function () {
      */
     $i.animate(centered, function () {
       setTimeout(function () {
-        $i.animate($.extend({
-          height: '35%',
-          width: '25%'
-        }, centered), 500, 'easeInOutQuint', function () {
+        /**
+         * Scale him up while keeping him in the center of the screen.
+         */
+        $i.animate({
+          height: $i.height() * scaling,
+          width: $i.width() * scaling,
+          top: (height / 2) - ($scaled.height() / 2),
+          left: (width / 2) - ($scaled.width() / 2)
+        }, 2000, 'easeOutExpo', function () {
           /**
-           * Scale him up while keeping him in the center of the screen.
+           * After some time, scale him back down
            */
-          $i.animate({
-            height: scale,
-            width: scale,
-            top: (height / 2) - ($scaled.height() / 2),
-            left: (width / 2) - ($scaled.width() / 2)
-          }, 500, 'easeOutExpo', function () {
-            /**
-             * After some time, scale him back down
-             */
-            setTimeout(function () {
-              $i.animate($.extend({
-                height: iHeight,
-                width: iWidth
-              }, centered), 3000);
-            }, 2000);
-          });
+          setTimeout(function () {
+            $i.animate($.extend({
+              height: iHeight,
+              width: iWidth
+            }, centered), 3000);
+          }, 2000);
         });
       }, 500);
     });
