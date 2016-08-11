13b61fd52b86f04f38b96a61e8ccfba62ef3f3b0
Greg Goforth <greg.goforth@gmail.com>
2016-08-10 21:49:26 -0700

    adds city background

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
   router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Express'});
+  res.render('index', {title: 'AlexaMan'});
 });
 
 router.get('/superman', (req, res, next) => {
